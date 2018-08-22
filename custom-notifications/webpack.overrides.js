458b2e90141ea7f385014940a1ee922489f421f8
Marvin <mreyes@i-sight.com>
2018-08-22 08:21:46 -0400

    updated overrides

--- custom-notifications/webpack.overrides.js
+++ custom-notifications/webpack.overrides.js
@@ -7,7 +7,9 @@ var overrides = {
 	'isight/public/lib/escalations.hack.js': 'public/lib/escalations.hack-ex.js',
 	'isight/public/views/settings/case-notification/case-notification-details-view.js':
 		'public/views/settings/case-notification/case-notification-details-view-ex.js',
-	'isight/public/models/trigger-model.js': 'public/models/trigger-model-ex.js'
+	'isight/public/models/trigger-model.js': 'public/models/trigger-model-ex.js',
+	'isight/public/views/settings/picklist-settings-view.js':
+		'public/views/settings/picklist-settings-view-ex.js'
 };
 
 module.exports = overrides;
